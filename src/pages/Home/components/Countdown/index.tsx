d4dfccd2fb3b5be556f038562a4445174ba28e08
Matheus Barbosa <mamateubosa00@gmail.com>
2025-03-13 19:04:29 -0300

    refactor : Home page; extract NewCycleForm and Countdown components for better structure

---
+++ src/pages/Home/components/Countdown/index.tsx
@@ -0,0 +1,13 @@
+import { CountdownConatiner, Separator } from './styles'
+
+export function Countdown() {
+  return (
+    <CountdownConatiner>
+      <span>{minutes[0]}</span>
+      <span>{minutes[1]}</span>
+      <Separator>:</Separator>
+      <span>{seconds[0]}</span>
+      <span>{seconds[1]}</span>
+    </CountdownConatiner>
+  )
+}
