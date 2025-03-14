d77aa7227ee32cfcfd9e2ea08fd27aae9a6dd078
Matheus Barbosa <mamateubosa00@gmail.com>
2025-03-14 11:24:38 -0300

    feat: implement CyclesContext for managing cycles state; refactor components to use context

--- src/pages/Home/components/Countdown/index.tsx
+++ src/pages/Home/components/Countdown/index.tsx
@@ -1,7 +1,7 @@
 import { differenceInSeconds } from 'date-fns'
 import { useContext, useEffect } from 'react'
-import { CyclesContext } from '../..'
 import { CountdownConatiner, Separator } from './styles'
+import { CyclesContext } from '../../../../contexts/CyclesContext'
 
 export function Countdown() {
   const {
